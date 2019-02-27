732b08b586f2a749df5334f0dbcb0074fb417068
YuanqiangLiu <yuanqiangliu25@gmail.com>
2019-02-27 16:55:06 +0800

    init ncnn_optee

---
+++ ta/test.c
@@ -0,0 +1,9 @@
+//#include "test.h"
+//#include <iostream>
+#include <stdio.h>
+
+void testFun(void)
+{
+//	std::cout<<"Implementing TA by C++"<<std::endl;
+	printf("Implementing TA by C++\n");
+}
---
+++ ta/test.h
@@ -0,0 +1,6 @@
+#ifndef _CPP_TEST_H__
+#define _CPP_TEST_H__
+
+extern "C" void testFun();
+
+#endif
